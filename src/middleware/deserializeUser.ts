1436d18d3eb199a7d3d006f157293bf52e96cef9
SerhatG35 <genc.serhat97@gmail.com>
2022-01-15 20:40:15 +0300

    commit

--- src/middleware/deserializeUser.ts
+++ src/middleware/deserializeUser.ts
@@ -38,7 +38,7 @@ const deserializeUser = async (
         httpOnly: true,
         domain: config.get("domain"),
         path: "/",
-        sameSite: "none",
+        sameSite: "strict",
         secure: config.get("secure"),
       });
     }
