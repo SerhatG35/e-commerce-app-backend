916a3e3fb543c645222ea4b389cef35d005f9475
SerhatG35 <genc.serhat97@gmail.com>
2022-02-10 20:53:23 +0300

    commit

--- src/middleware/requireUser.ts
+++ src/middleware/requireUser.ts
@@ -2,6 +2,7 @@ import { Request, Response, NextFunction } from "express";
 
 const requireUser = (req: Request, res: Response, next: NextFunction) => {
   const user = res.locals.user;
+console.log(res.locals);
 
   if (!user) return res.status(403).send("Unauthorized request please login");
 
